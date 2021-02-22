8d763094dedd274c21d73f94e0062d086eaa9219
zkazemy <zahrakazemy@gmail.com>
2021-02-22 13:21:33 +0330

    Java Bean Configuration and XML Configuration

--- src/main/java/zkazemy/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/zkazemy/springframework/joke/services/JokeServiceImpl.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Service;
 public class JokeServiceImpl implements JokeService {
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
+//    public JokeServiceImpl() {
+//        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+//    }
 
     @Override
     public String getJoked() {
